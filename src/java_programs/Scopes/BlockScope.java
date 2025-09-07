c50538811f5c69bc96d07628201f09799476f5fe
chiranth chiru <chiranth2005m@gmail.com>
2025-09-07 19:36:34 +0530

    Java

--- src/java_programs/Scopes/BlockScope.java
+++ src/java_programs/Scopes/BlockScope.java
@@ -1,4 +1,22 @@
 package java_programs.Scopes;
 
 public class BlockScope {
+    public static void main(String[] args) {
+        int a =10;
+        int b =20;
+        {
+            // int a = 30  --> Error -> a is already initialized outside the block in the same method, so cannot reinitialize it
+            System.out.println(a);
+            a = 40;
+            System.out.println(a);
+
+            int c = 30;
+            System.out.println(c);
+
+            // Values initialized in this block will remain in the block
+
+        }
+        // System.out.println(c); --> Error -> c cant be accessed outside the block
+        System.out.println(a+ " "+b);
+    }
 }
