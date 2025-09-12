8b493e4b8a35306e48d4c1db5aefdf8f01ac175a
chiranth chiru <chiranth2005m@gmail.com>
2025-09-12 19:52:46 +0530

    Java

---
+++ src/java_programs/LINEAR_SEARCH/SearchEvenDigits.java
@@ -0,0 +1,38 @@
+package java_programs.LINEAR_SEARCH;
+
+public class SearchEvenDigits {
+    public static void main(String[] args) {
+        int[] nums = {12,345,2,6,7896};
+        System.out.println(findNumbers(nums));
+
+    }
+    static int findNumbers(int[] nums) {
+        int count = 0;
+        for (int num:nums){
+            if(even(num)){
+                count++;
+            }
+        }
+        return count;
+    }
+    static boolean even(int num){
+        int numofDigits = digits(num);
+        return numofDigits % 2 ==0;
+    }
+    static int digits(int num){
+
+        if(num<0){
+            num = num * -1;
+        }
+        if(num==0){
+            return 1;
+        }
+        int count = 0;
+        while(num>0){
+            count++;
+            num = num/10;
+        }
+        return count;
+    }
+}
+
