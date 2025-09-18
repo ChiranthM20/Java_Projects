4cd33c35f7b1c2d6adae5f255f743e2cada9a6ce
chiranth chiru <chiranth2005m@gmail.com>
2025-09-18 19:07:16 +0530

    Java

---
+++ src/java_programs/ARRAYS/Practice_problems/TwoSum.java
@@ -0,0 +1,25 @@
+package java_programs.ARRAYS.Practice_problems;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] arr = {2,3,1,4,5,2,2,5,7,83,1,9};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(arr,target)));
+    }
+
+    static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};  // If no solution found
+    }
+}
