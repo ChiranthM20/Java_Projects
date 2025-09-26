b15a5e8f9bbacd15d65c134fe4a4dc90d3f4429a
chiranth chiru <chiranth2005m@gmail.com>
2025-09-26 18:38:16 +0530

    Java

---
+++ src/practice_problems/PowerOfTwo.java
@@ -0,0 +1,22 @@
+package practice_problems;
+
+import java.util.Scanner;
+
+public class PowerOfTwo {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number : ");
+        int n = input.nextInt();
+        System.out.println(isPowerOfTwo(n));
+
+    }
+    static boolean isPowerOfTwo(int n){
+        if(n<=0){
+            return false;
+        }
+        while(n%2==0){
+            n=n/2;
+        }
+        return n==1;
+    }
+}
