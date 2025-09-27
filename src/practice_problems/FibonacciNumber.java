2bd75fe2b90312075f747c96cd33e4e1756b0443
chiranth chiru <chiranth2005m@gmail.com>
2025-09-27 21:48:43 +0530

    Java

---
+++ src/practice_problems/FibonacciNumber.java
@@ -0,0 +1,19 @@
+package practice_problems;
+
+public class FibonacciNumber {
+    public static void main(String[] args) {
+        System.out.println(fibonacciNumber(3));
+    }
+    static int fibonacciNumber(int n){
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return b;
+    }
+}
