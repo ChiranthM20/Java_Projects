c37207f7f028e4fd450645229f206fd0c6c5adcd
Chiranth M <chiranth2005m@gmail.com>
2026-08-14 19:15:25 +0530

    Java

--- src/java_programs/OOPS/Constructors.java
+++ src/java_programs/OOPS/Constructors.java
@@ -13,12 +13,26 @@ public class Constructors {
 
         employee1.greeting();
 
+        Employee employee3 = new Employee();
+        System.out.println(employee3.name+" "+employee3.id+" "+employee3.salary);
+
+
+        Employee employee4 = employee2;
+        employee2.salary = 100000;
+        employee4.changeName("Human");
+        System.out.println(employee4.name+" "+employee4.id+" "+employee4.salary);
     }
 }
 class Employee{
     String name;
     int id;
     int salary;
+
+    Employee(){
+        name = "unknown";
+        id = 0;
+        salary = 0;
+    }
 
     void greeting(){
         System.out.println("Hello! My name is "+name);
