a458d537fb329261b7f0211c8f13486525b19ce3
Chiranth M <chiranth2005m@gmail.com>
2026-08-15 17:39:12 +0530

    Java

---
+++ src/java_programs/Packages/Package1/A/Greeting.java
@@ -0,0 +1,14 @@
+package java_programs.Packages.Package1.A;
+
+import java.util.Scanner;
+
+public class Greeting{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println ("Hello");
+        System.out.print("Please enter you name : ");
+        String name = input.nextLine();
+        System.out.println("Welcome "+name);
+    }
+}
