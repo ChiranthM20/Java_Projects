3dbb6721f14c9ae5a65b63e86e50e525ac4fcb04
Chiranth M <chiranth2005m@gmail.com>
2026-08-16 20:40:36 +0530

    Java

--- src/java_programs/OOPS/StaticExample/staticExample.java
+++ src/java_programs/OOPS/StaticExample/staticExample.java
@@ -3,8 +3,8 @@ package java_programs.OOPS.StaticExample;
 public class staticExample {
     public static void main(String[] args) {
         Student student1 = new Student(13, "Chiranth");
-        Student student2 = new Student(04, "Adithya");
-        Student student3 = new Student(07, "Antony Kevin");
+        Student student2 = new Student(4, "Adithya");
+        Student student3 = new Student(7, "Antony Kevin");
         Student student4 = new Student(70, "Srujan");
 
         student1.display();
@@ -12,7 +12,10 @@ public class staticExample {
         student3.display();
         student4.display();
 
-        // Year and college name is same for everyone because they are from same college and same year
+        System.out.println("");
+        System.out.println("Hello "+student1.name+" there are "+ Student.population +" Students in your class");
+
+        // Year and college name is same for everyone because they are from same college and same yeargit ststus
     }
 }
 class Student {
@@ -20,13 +23,16 @@ class Student {
     String name;
     static int year;
     static String college;
+    static int population;
 
     Student(int rollno, String name) {
         this.rollno = rollno;
         this.name = name;
         Student.year = 4;
         Student.college = "Garden City University";
+        Student.population +=1;
     }
+
 
     void display(){
         System.out.println("Name : "+name+", Rollno : "+rollno+", Year : "+year+", College : "+college);
